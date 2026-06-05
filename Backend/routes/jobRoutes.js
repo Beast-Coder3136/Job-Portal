0b6b69d977e4510361252b4817e3d78e3b0a50e9
Anas <mohdanasali3136@gamil.com>
2026-06-05 16:38:58 +0530

    Some Updates

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -4,7 +4,7 @@ import verifyJWT from '../middlewares/authentication.js';
 import checkRole from '../middlewares/checkRole.js';
 const router = express.Router();
 
-router.get("/get",verifyJWT,getAllJobs);
+router.get("/get",verifyJWT,getAllJobs)
 router.post("/post",verifyJWT,checkRole("recruiter"),postJob);
 router.get("/get/:id",verifyJWT,getJobById);
 router.get("/adminJob",verifyJWT,checkRole("recruiter"),getAllJobByAdmin);
